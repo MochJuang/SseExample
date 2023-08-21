55b6082557775cab3bffe1e7e5ee5eb9ea09ea6d
mochjuangpp <mochjuangpp@gmail.com>
2023-08-21 22:32:46 +0700

    add unit test

---
+++ main_test.go
@@ -0,0 +1,20 @@
+package main_test
+
+import (
+	code "go-sse"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("negative test", func(t *testing.T) {
+		result := code.Sum(2, 2)
+		assert.NotEqual(t, result, 4)
+	})
+
+	t.Run("positive test", func(t *testing.T) {
+		result := code.Sum(2, 2)
+		assert.Equal(t, result, 4)
+	})
+}
